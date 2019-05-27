73a1b91cc7b4c67b76d4cb8c7cdfa1a6d9bf1690
M.Daneshjoo <47611839+mdaneshjoo@users.noreply.github.com>
2019-05-27 14:07:19 +0430

    dynamic programming function added

--- src/main.java
+++ src/main.java
@@ -11,14 +11,35 @@ class CatalnNumber {
         }
         for (int i = 0; i < n; i++) {
             res += catalan(i) * catalan(n - i - 1);
-        }
+    }
         return res;
     }
-
+   public static long catalan_dynamic(int n){
+        int i, j;
+       long[] catalan = new long[n+1];
+       catalan[0]=1;
+        for(i = 1 ; i <= n ; i++){
+            catalan[i] = 0;
+            for(j = 0 ; j < i ; j++)
+                catalan[i] += catalan[j] * catalan[i - j - 1];
+        }
+        return catalan[n];
+    }
     public static void main(String[] args) {
         CatalnNumber cn = new CatalnNumber();
         for (int i = 0; i < 10; i++) {
             System.out.print(cn.catalan(i) + " ");
+
         }
+
+        System.out.print("\n");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(catalan_dynamic(i) + " ");
+
+        }
+
     }
+
+
 }
